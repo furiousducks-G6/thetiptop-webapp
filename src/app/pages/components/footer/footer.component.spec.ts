410ca80dbd8fc2f76351625aadc226dc6bf495ac
Morin Joane KENMEGNI <morin-joane.kenmegni@devpoint.fr>
2024-09-15 11:17:16 +0200

    site map modifier

--- src/app/pages/components/footer/footer.component.spec.ts
+++ src/app/pages/components/footer/footer.component.spec.ts
@@ -1,13 +1,13 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { FooterComponent } from './footer.component';
-import { FormsModule } from '@angular/forms'; // Importation de FormsModule
+import { FormsModule } from '@angular/forms'; 
 import { ViewportScroller } from '@angular/common';
 import { Router, NavigationEnd } from '@angular/router';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { of } from 'rxjs';
 import { PLATFORM_ID } from '@angular/core';
-import axios from 'axios'; // Assurez-vous d'importer axios ici
-import { fakeAsync, tick } from '@angular/core/testing';
+import axios from 'axios';
+
 describe('FooterComponent', () => {
   let component: FooterComponent;
   let fixture: ComponentFixture<FooterComponent>;
@@ -16,7 +16,7 @@ describe('FooterComponent', () => {
 
   beforeEach(async () => {
     routerMock = {
-      events: of(new NavigationEnd(0, '', '')) // Importation correcte de NavigationEnd
+      events: of(new NavigationEnd(0, '', '')) 
     };
 
     viewportScrollerMock = {
@@ -25,11 +25,11 @@ describe('FooterComponent', () => {
 
     await TestBed.configureTestingModule({
       declarations: [FooterComponent],
-      imports: [HttpClientTestingModule, FormsModule], // Ajout de FormsModule ici
+      imports: [HttpClientTestingModule, FormsModule],
       providers: [
         { provide: ViewportScroller, useValue: viewportScrollerMock },
         { provide: Router, useValue: routerMock },
-        { provide: PLATFORM_ID, useValue: 'browser' } // Spécifier que le test s'exécute dans un environnement de navigateur
+        { provide: PLATFORM_ID, useValue: 'browser' }
       ]
     }).compileComponents();
 
@@ -60,38 +60,12 @@ describe('FooterComponent', () => {
     expect(component.showNotificationMessage).toHaveBeenCalledWith('Veuillez entrer une adresse e-mail valide.', 'error');
   });
 
-  it('should show success message after valid subscription', (done) => {
-    spyOn(axios, 'post').and.returnValue(Promise.resolve({ status: 200 }));
-    spyOn(component, 'showNotificationMessage');
-
-    component.email = 'test@example.com';
-    component.subscribeToNewsletter();
-
-    setTimeout(() => {
-      expect(component.showNotificationMessage).toHaveBeenCalledWith('Merci pour votre inscription à notre newsletter!', 'success');
-      done();
-    }, 100);
-  });
-
-  it('should show error message on subscription failure', (done) => {
-    spyOn(axios, 'post').and.returnValue(Promise.reject({}));
-    spyOn(component, 'showNotificationMessage');
-
-    component.email = 'test@example.com';
-    component.subscribeToNewsletter();
-
-    setTimeout(() => {
-      expect(component.showNotificationMessage).toHaveBeenCalledWith('Une erreur s\'est produite lors de l\'inscription. Veuillez réessayer.', 'error');
-      done();
-    }, 100);
-  });
 
   it('should hide notification after 5 seconds', fakeAsync(() => {
     component.showNotificationMessage('Test Message', 'success');
     expect(component.showNotification).toBeTrue();
   
-    // Simule le passage de 5 secondes
-    tick(5000);
+    tick(5000); // Simule le passage de 5 secondes
   
     expect(component.showNotification).toBeFalse();
   }));
